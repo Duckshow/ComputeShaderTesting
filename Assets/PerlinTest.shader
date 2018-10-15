43ac9c03ed93ffb445dcc8d9b9d5006f1bbd24ac
Stefan Andersson <stefan@tocaboca.com>
2018-10-15 08:21:10 +0200

    Started working on an alternative to simulation

---
+++ Assets/PerlinTest.shader
@@ -0,0 +1,64 @@
+﻿Shader "Unlit/PerlinTest"{
+	Properties{
+		_PerlinTex_0 ("PerlinTexture_0", 2D) = "white" {}
+		_PerlinTex_1 ("PerlinTexture_1", 2D) = "white" {}
+		_PerlinTex_2 ("PerlinTexture_2", 2D) = "white" {}
+	}
+	SubShader{
+		Tags { "RenderType"="Opaque" }
+
+		Pass{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata{
+				float4 vertex : POSITION;
+				fixed2 uv : TEXCOORD0;
+				fixed color : COLOR;
+			};
+
+			struct v2f{
+				float4 vertex : SV_POSITION;
+				fixed2 uv : TEXCOORD0;
+				fixed color : COLOR;
+			};
+
+			sampler2D _PerlinTex_0;
+			sampler2D _PerlinTex_1;
+			sampler2D _PerlinTex_2;
+			float4 _PerlinTex_0_ST;
+			
+			v2f vert (appdata v){
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _PerlinTex_0);
+				o.color = v.color;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target{
+				i.uv.x = round(i.uv.x * 32) / 32.0;
+				i.uv.y = round(i.uv.y * 32) / 32.0;
+
+				fixed vColor = round(i.color * 32) / 32.0;
+
+				fixed perlin_0 = tex2D(_PerlinTex_0, i.uv);
+				fixed perlin_1 = tex2D(_PerlinTex_1, i.uv);
+				fixed perlin_2 = tex2D(_PerlinTex_2, i.uv);
+
+				half time = (_Time * 5.0/*0.33*/) % 1.0;
+
+				fixed perlinFinal = perlin_0;
+				perlinFinal = lerp(perlinFinal, perlin_1, max(0.0, time - 0.0) / 0.33);
+				perlinFinal = lerp(perlinFinal, perlin_2, max(0.0, time - 0.33) / 0.33);
+				perlinFinal = lerp(perlinFinal, perlin_0, max(0.0, time - 0.66) / 0.33);
+				
+				return fixed4(step(perlinFinal, vColor), 0.0, 0.0, 1.0);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/PerlinTextureGenerator.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+
+
+public class PerlinTextureGenerator : EditorWindow {
+	private const float SCALE_MOD = 500.0f;
+	private const string PATH_DIRECTORY = "Assets/PerlinTextures/";
+
+	private float perlinScale = 1.0f;
+	private int seed = 0;
+	private int width = 32;
+	private int height = 32;
+	private Vector2 scrollPos;
+
+
+
+	[MenuItem("Tools/Generate Perlin Texture")]
+	public static void ShowWindow(){
+		GetWindow(typeof(PerlinTextureGenerator));
+	}
+
+	void OnGUI(){
+		if (Application.isPlaying) return;
+
+		seed = EditorGUILayout.IntField("Seed", seed);
+		if (GUILayout.Button("Randomize Seed")){
+			seed = Random.Range(0, 1000000);
+		}
+
+		GUILayout.Space(10);
+		GUILayout.Label("Please only use power-of-two values!");
+		width = EditorGUILayout.IntField("Width", width);
+		height = EditorGUILayout.IntField("Height", height);
+		
+		GUILayout.Space(10);
+		perlinScale = EditorGUILayout.Slider("Scale", perlinScale * SCALE_MOD, 0.0f, 100.0f) / SCALE_MOD;
+		
+		if (GUILayout.Button("Create")) Create();
+	}
+
+	public void Create(){
+		Texture2D texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+		Color32[] pixels = new Color32[width * height];
+		int i = 0;
+		for (int x = 0; x < width; x++){
+			for (int y = 0; y < height; y++){
+				EditorUtility.DisplayProgressBar("Perlin Texture Generator", "Generating...", ((float)i / (float)(width * height)));
+				byte perlin = (byte)(Mathf.PerlinNoise((seed + x) * perlinScale, (seed + y) * perlinScale) * 255);
+				pixels[y * width + x] = new Color32(perlin, 0, 0, 255);
+				i++;
+			}
+		}
+		EditorUtility.ClearProgressBar();
+		texture.SetPixels32(pixels);
+
+		if (!Directory.Exists(PATH_DIRECTORY)) Directory.CreateDirectory(PATH_DIRECTORY);
+
+		string textureFileName = "";
+		string texturePath = "";
+		int iterations = 0;
+		do{
+			textureFileName = "perlin_" + iterations.ToString();
+			texturePath = PATH_DIRECTORY + textureFileName + ".png";
+			iterations++;
+		}
+		while (File.Exists(texturePath));
+
+		byte[] png = texture.EncodeToPNG();
+		string pngPath = Application.dataPath.Replace("Assets", "") + texturePath;
+		File.WriteAllBytes(pngPath, png);
+
+		AssetDatabase.SaveAssets();
+		AssetDatabase.Refresh();
+
+		TextureImporter importer = (TextureImporter)TextureImporter.GetAtPath(texturePath);
+		importer.alphaIsTransparency = true;
+		importer.filterMode = FilterMode.Point;
+		importer.isReadable = true;
+		importer.mipmapEnabled = false;
+		importer.wrapMode = TextureWrapMode.Clamp;
+		importer.maxTextureSize = Mathf.Max(width, height);
+		importer.SaveAndReimport();
+
+		Debug.Log("Texture has been created!");
+
+		AssetDatabase.SaveAssets();
+		AssetDatabase.Refresh();
+	}
+}
