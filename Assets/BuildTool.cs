8c9fd20de373402402bd37dd104905ee4aa65bfa
Stefan Andersson <stefan@tocaboca.com>
2018-10-22 08:28:45 +0200

    Started fiddling with a buildtool

---
+++ Assets/BuildTool.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BuildTool : Singleton<BuildTool> {
+
+	private Mouse instanceMouse;
+
+	private Vector2Int posGridStart;
+	private Vector2Int posGridEnd;
+
+
+	public override bool IsUsingAwakeDefault() { return true; }
+	public override void AwakeDefault() {
+		instanceMouse = Mouse.GetInstance();
+	}
+
+	public override bool IsUsingUpdateDefault() { return true; }
+	public override void UpdateDefault() { 
+		if (Mouse.GetInstance().GetStateLMB() == Mouse.StateEnum.Click){
+			posGridStart = instanceMouse.GetPosGrid();
+		}
+		else if (Mouse.GetInstance().GetStateLMB() == Mouse.StateEnum.Hold){
+			posGridEnd = instanceMouse.GetPosGrid();
+		
+			
+		}
+	}
+}
