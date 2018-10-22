95012e5b56466921ccf8355a6ab4726eb44908ef
Stefan Andersson <stefan@tocaboca.com>
2018-10-22 08:29:27 +0200

    Tweaked ShipMesh

--- Assets/ShipMesh.cs
+++ Assets/ShipMesh.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 
-public class ShipMesh : MonoBehaviour {
+public class ShipMesh : Singleton<ShipMesh> {
 
 	private const int PIXELS_PER_UNIT = 32;
+	private const int SIZE_TILE = 1;
 
 	private const int VERTICES_PER_TILE = 5;
 	private const int TRIS_PER_TILE = 4;
@@ -23,8 +24,7 @@ public class ShipMesh : MonoBehaviour {
 	[SerializeField] private RenderTexture texturePixellation;
 	[SerializeField] private Transform meshPixellation;
 	[Space]
-	[SerializeField] private Vector2Int sizeShip;
-	[SerializeField] private Vector2Int sizeTile;
+	[SerializeField] private Int2 size;
 	[SerializeField] private float scaleElements = 0.125f;
 	[SerializeField] private float scaleTime = 2.0f;
 
@@ -40,38 +40,42 @@ public class ShipMesh : MonoBehaviour {
 	private int propertyID_ScaleTime;
 
 
+	public Int2 GetWorldSize() {
+		return size * SIZE_TILE;
+	}
+
 	[EasyButtons.Button]
 	public void Generate(){
 		meshRenderer = GetComponentInChildren<MeshRenderer>();
 		meshFilter = GetComponentInChildren<MeshFilter>();
 		mesh = new Mesh();
 
-		int vertexCount = sizeShip.x * sizeShip.y * VERTICES_PER_TILE;
+		int vertexCount = size.x * size.y * VERTICES_PER_TILE;
 
 		Vector3[] vertices = new Vector3[vertexCount];
 		Vector2[] uvTexture = new Vector2[vertexCount];
 		Vector2[] uvPerlin = new Vector2[vertexCount];
-		int[] tris = new int[sizeShip.x * sizeShip.y * TRIS_PER_TILE * 3];
+		int[] tris = new int[size.x * size.y * TRIS_PER_TILE * 3];
 		
 		vertexColors = new Color32[vertexCount];
 		for (int i = 0; i < vertexColors.Length; i++){
 			vertexColors[i] = new Color32(0, 0, 0, 255);
 		}
 
-		Vector3 offset = new Vector3(sizeShip.x / 2, sizeShip.y / 2, 0) * -1;
+		Vector3 offset = new Vector3(size.x / 2, size.y / 2, 0) * -1;
 
 		int vIndex = 0;
 		int tIndex = 0;
-		for (int y = 0; y < sizeShip.y; y++){
-			for (int x = 0; x < sizeShip.x; x++){
+		for (int y = 0; y < size.y; y++){
+			for (int x = 0; x < size.x; x++){
 				int vIndexBottomLeft = vIndex + VERTEX_INDEX_BOTTOM_LEFT;
 				int vIndexTopLeft = vIndex + VERTEX_INDEX_TOP_LEFT;
 				int vIndexTopRight = vIndex + VERTEX_INDEX_TOP_RIGHT;
 				int vIndexBottomRight = vIndex + VERTEX_INDEX_BOTTOM_RIGHT;
 				int vIndexCenter = vIndex + VERTEX_INDEX_CENTER;
 
-				float sizeShipX = (float)sizeShip.x;
-				float sizeShipY = (float)sizeShip.y;
+				float sizeShipX = (float)size.x;
+				float sizeShipY = (float)size.y;
 				Vector2 uvTextureMin = Vector2.zero;
 				Vector2 uvTextureMax = Vector2.one;
 				Vector2 uvPerlinMin = new Vector2(x / sizeShipX, y / sizeShipY);
@@ -82,19 +86,19 @@ public class ShipMesh : MonoBehaviour {
 				uvTexture[vIndexBottomLeft] = uvTextureMin;
 				uvPerlin[vIndexBottomLeft] = uvPerlinMin;
 
-				vertices[vIndexTopLeft] = offset + new Vector3(x, y + sizeTile.y, 0.0f);
+				vertices[vIndexTopLeft] = offset + new Vector3(x, y + SIZE_TILE, 0.0f);
 				uvTexture[vIndexTopLeft] = new Vector2(uvTextureMin.x, uvTextureMax.y);
 				uvPerlin[vIndexTopLeft] = new Vector2(uvPerlinMin.x, uvPerlinMax.y);
 
-				vertices[vIndexTopRight] = offset + new Vector3(x + sizeTile.x, y + sizeTile.y, 0.0f);
+				vertices[vIndexTopRight] = offset + new Vector3(x + SIZE_TILE, y + SIZE_TILE, 0.0f);
 				uvTexture[vIndexTopRight] = uvTextureMax;
 				uvPerlin[vIndexTopRight] = uvPerlinMax;
 
-				vertices[vIndexBottomRight] = offset + new Vector3(x + sizeTile.x, y, 0.0f);
+				vertices[vIndexBottomRight] = offset + new Vector3(x + SIZE_TILE, y, 0.0f);
 				uvTexture[vIndexBottomRight] = new Vector2(uvTextureMax.x, uvTextureMin.y);
 				uvPerlin[vIndexBottomRight] = new Vector2(uvPerlinMax.x, uvPerlinMin.y);
 
-				vertices[vIndexCenter] = offset + new Vector3(x + sizeTile.x * 0.5f, y + sizeTile.y * 0.5f, 0.0f);
+				vertices[vIndexCenter] = offset + new Vector3(x + SIZE_TILE * 0.5f, y + SIZE_TILE * 0.5f, 0.0f);
 				uvTexture[vIndexCenter] = uvTextureMax * 0.5f;
 				uvPerlin[vIndexCenter] = uvPerlinCenter;
 
@@ -116,7 +120,7 @@ public class ShipMesh : MonoBehaviour {
 						vertexColors[vIndexTopRight - VERTICES_PER_TILE] = newColor;
 						vertexColors[vIndexCenter - VERTICES_PER_TILE] = newColor;
 					} 
-					if (x < sizeShip.x - 1){
+					if (x < size.x - 1){
 						vertexColors[vIndexBottomLeft + VERTICES_PER_TILE] = newColor;
 						vertexColors[vIndexTopLeft + VERTICES_PER_TILE] = newColor;
 						vertexColors[vIndexCenter + VERTICES_PER_TILE] = newColor;
@@ -153,20 +157,20 @@ public class ShipMesh : MonoBehaviour {
 		mesh.RecalculateBounds();
 		meshFilter.mesh = mesh;
 
-		Vector2Int sizeShipPixels = sizeShip * PIXELS_PER_UNIT;
+		Int2 sizeShipPixels = size * PIXELS_PER_UNIT;
 		if (sizeShipPixels.x != texturePixellation.width || sizeShipPixels.y != texturePixellation.height){
 			Debug.LogErrorFormat("TexturePixellation's size has to be set manually! Correct size is X: {0}, Y: {1}", sizeShipPixels.x, sizeShipPixels.y);
 		}
-		cameraPixellation.orthographicSize = sizeShip.y / 2;
-		meshPixellation.localScale = new Vector3(sizeShip.x, sizeShip.y, 1);
+		cameraPixellation.orthographicSize = size.y / 2;
+		meshPixellation.localScale = new Vector3(size.x, size.y, 1);
 
 		propertyID_ScaleShipX = Shader.PropertyToID(PROPERTY_SCALESHIPX);
 		propertyID_ScaleShipY = Shader.PropertyToID(PROPERTY_SCALESHIPY);
 		propertyID_ScaleElements = Shader.PropertyToID(PROPERTY_SCALEELEMENTS);
 		propertyID_ScaleTime = Shader.PropertyToID(PROPERTY_SCALETIME);
 
-		material.SetFloat(propertyID_ScaleShipX, sizeShip.x);
-		material.SetFloat(propertyID_ScaleShipY, sizeShip.y);
+		material.SetFloat(propertyID_ScaleShipX, size.x);
+		material.SetFloat(propertyID_ScaleShipY, size.y);
 		material.SetFloat(propertyID_ScaleElements, scaleElements);
 		material.SetFloat(propertyID_ScaleTime, scaleTime);
 	}
