061beb85e3365fdcee2af20ef6db25265d22291e
Stefan Andersson <stefan@tocaboca.com>
2018-10-22 08:27:14 +0200

    Added Int2 and Float2

--- Assets/Extensions.cs
+++ Assets/Extensions.cs
@@ -12,3 +12,51 @@ public static class Extensions {
 		if(axisIndex == 2) v.z = value;
 	}
 }
+
+[System.Serializable] public struct Float2 {
+	public float x;
+	public float y;
+
+	public Float2(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public static Float2 operator +(Float2 value0, Float2 value1){
+		return new Float2(value0.x + value1.x, value0.y + value1.y);
+	}
+
+	public static Float2 operator -(Float2 value0, Float2 value1){
+		return new Float2(value0.x - value1.x, value0.y - value1.y);
+	}
+	
+	public static Float2 operator *(Float2 value0, int m) {
+		return new Float2(value0.x * m, value0.y * m);
+	}
+}
+
+[System.Serializable] public struct Int2 {
+	public int x;
+	public int y;
+
+	public Int2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public Int2(float x, float y) {
+		this.x = (int)x;
+		this.y = (int)y;
+	}
+
+	public static Int2 operator +(Int2 value0, Int2 value1){
+		return new Int2(value0.x + value1.x, value0.y + value1.y);
+	}
+
+	public static Int2 operator -(Int2 value0, Int2 value1){
+		return new Int2(value0.x - value1.x, value0.y - value1.y);
+	}
+
+	public static Int2 operator *(Int2 value0, int m) {
+		return new Int2(value0.x * m, value0.y * m);
+	}
+}
